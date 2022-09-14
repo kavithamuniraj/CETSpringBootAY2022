2312633920d225563ceb4c5b326bf07485d74459
kavithamuniraj <64585038+kavithamuniraj@users.noreply.github.com>
2022-09-14 13:26:11 +0800

    Add files via upload

---
+++ day2/code/BooklinkSBS/java/com/sbstransit/booklinkSBS/BooklinkSbsApplication.java
@@ -0,0 +1,13 @@
+package com.sbstransit.booklinkSBS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklinkSbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklinkSbsApplication.class, args);
+	}
+
+}
