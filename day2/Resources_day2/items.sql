dc48df9108e160e0207199fc36f2a7776ea82021
kavithamuniraj <64585038+kavithamuniraj@users.noreply.github.com>
2022-09-14 12:55:04 +0800

    Add files via upload

---
+++ day2/Resources_day2/items.sql
@@ -0,0 +1,4 @@
+INSERT INTO `item` (`id`, `description`, `name`, `price`, `quantity`, `category_id`) VALUES (1,'Strawberry and chocolate','Ice Cream Bar',1.5,10, 2);
+INSERT INTO `item` (`id`, `description`, `name`, `price`, `quantity`, `category_id`) VALUES (2,'Pain and headache medication','Panadol',5.5,5, 3);
+INSERT INTO `item` (`id`, `description`, `name`, `price`, `quantity`, `category_id`) VALUES (3,'Red pen','Pen',0.8,15, 1);
+INSERT INTO `item` (`id`, `description`, `name`, `price`, `quantity`, `category_id`) VALUES (4,'A set of 5 colourful highlighters','Highlighter',4.5,3,1);
